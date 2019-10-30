3bfcfec67da2b8996ae217bd8a15f9d5656f7dde
Katarina Käll <katarina_kall86@hotmail.com>
2019-10-30 14:01:48 +0100

    Added methods in game class

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -2,19 +2,21 @@ package com.company;
 
 public class Game implements Runnable{
     private Display display;
+    public String title;
     public int width, height;
 
     private boolean running = false;
     private Thread thread;
 
     public Game(String title, int width, int height){
+        this.title = title;
         this.width = width;
         this.height = height;
 
-        display = new Display(title, width, height);
     }
 
     private void init(){
+        display = new Display(title, width, height);
     }
 
     private void tick(){
@@ -28,18 +30,28 @@ public class Game implements Runnable{
     public void run(){
         init();
 
-        while(true){
+        while(running){
             tick();
             render();
         }
+
+        stop();
     }
 
     public synchronized void start(){
+        if(running)
+            return;
+
+        running = true;
         thread = new Thread(this);
         thread.start();
     }
 
     public synchronized void stop(){
+        if(!running)
+            return;
+
+        running = false;
         try {
             thread.join();
         } catch (InterruptedException e) {
