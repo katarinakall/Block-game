0e29907a36513de484738f081c1690ddfe88a446
Katarina Käll <katarina.kall@outlook.com>
2019-11-12 19:52:47 +0100

    Added a world class

--- src/com/company/GameState.java
+++ src/com/company/GameState.java
@@ -5,19 +5,23 @@ import java.awt.*;
 public class GameState extends State {
 
     private Player player;
+    private World world;
 
     public GameState(Game game) {
         super(game);
         player = new Player(game,100, 100);
+        world = new World("");
     }
 
     @Override
     public void tick() {
+        world.tick();
         player.tick();
     }
 
     @Override
     public void render(Graphics g) {
+        world.render(g);
         player.render(g);
     }
 }
